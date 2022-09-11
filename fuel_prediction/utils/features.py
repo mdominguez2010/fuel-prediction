ff87952a188daf047a9aeb9a47ea8e44de82a935
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-09-11 15:33:56 +0000

    update training pipeline

--- fuel_prediction/utils/features.py
+++ fuel_prediction/utils/features.py
@@ -21,8 +21,6 @@ from sklearn.pipeline import FeatureUnion
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 
-# from scripts.scrape_data import GasPipeline
-
 ######## TODO: BUILD ########
 # Do we need this?????
 #############################
@@ -36,6 +34,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 def clean_and_organize_data(data_dict: dict) -> list:
+    LOGGER.info("Cleaning and organizing data...")
     df = pd.DataFrame(data = data_dict)
     df.sort_values(by = ['year', 'period'], axis = 0, ascending = True, inplace = True)
     df = df[['date', 'value']]
@@ -44,26 +43,27 @@ def clean_and_organize_data(data_dict: dict) -> list:
     return df
 
 
-def windowed_dataset(series: list, window_size: int, batch_size: int, shuffle_buffer: int) -> tensorflow.python.data.ops.dataset_ops.PrefetchDataset:
+def windowed_dataset(series: list, window_size: int, batch_size: int, shuffle_buffer: int) -> tf.python.data.ops.dataset_ops.PrefetchDataset:
 	"""
 	We create time windows to create X and y features.
 	For example, if we choose a window of 20, we will create a dataset formed by 20 points as X
 	"""
-	dataset = tf.data.Dataset.from_tensor_slices(series)
-	dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-	dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
-	dataset = dataset.shuffle(shuffle_buffer)
-	dataset = dataset.map(lambda window: (window[:-1], window[-1]))
-	dataset = dataset.batch(batch_size).prefetch(1)
-	return dataset
+    LOGGER.info("Building windowed dataset...")
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
 
 
 def train_test_val_split(series: list) -> list:
     """
     Input: X --> array of features, set aside for validating/testing.
     Output: Features and target split into train, val and test sets. 
-            Test size = 20%
-            Val size = 20%
+    Test size = 20%
+    Val size = 20%
     """
     series, series_test = train_test_split(series, test_size=0.2, random_state=51, shuffle = False)
     series_train, series_val = train_test_split(series, test_size=0.2, random_state=51, shuffle = False)
